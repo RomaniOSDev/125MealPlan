b2bb339e6e69363ea4f27bfff997459b6aefdbe2
Roman <romazan.26@gmail.com>
2026-04-08 13:39:05 +0300

    Initial Commit

---
+++ 125MealPlan/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  125MealPlan
+//
+//  Created by Роман Главацкий on 08.04.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
